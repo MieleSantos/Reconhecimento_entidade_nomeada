38478bcaaa3a7a8a9529bbca539c2f643a28bc44
mielesantos <mielesnts@gmail.com>
2020-07-29 20:28:52 -0300

    adicinaando novas funcionalidades

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import json
 from flask import Flask, jsonify, request, render_template, redirect, url_for
 from flaskext.markdown import Markdown
 
-from process_entity_named import process_entity
+from process_entity_named import process_entity, process_parse_dependency
 
 
 HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; border-radius: 0.25rem; padding: 1rem">{}</div>"""
@@ -33,6 +33,23 @@ def process():
 def result():
     return render_template('result.html')
 
+
+@app.route("/result_parse", methods=['GET','POST'])
+def process_parse():
+
+    if request.method == 'POST':
+
+        query = request.form['query']    
+        
+        obj_html = process_parse_dependency(query)
+        result = HTML_WRAPPER.format(obj_html)
+
+
+    return render_template('result_parse.html', query=query ,result=result)
+
+@app.route("/parse_dep")
+def parse_dep():
+    return render_template('parse_dep.html')
 
 
 if __name__ == '__main__':
--- process_entity_named.py
+++ process_entity_named.py
@@ -3,16 +3,44 @@ from spacy import   displacy
 
 
 def process_entity(texto):
-    
-    nlp = spacy.load('pt')
-    
+    """[summary]
+        Função para retorna o reconhecimento das entidades nemeadas do texto processado
+    Args:
+        texto (string): Texto enviado para ser processado
+
+    Returns:
+        [Objeto]: Objeto html gerado pelo displacy.render
+    """
+    nlp = obj_nlp()
     doc = nlp(texto)
 
-    # for entidades in doc.ents:
-    # print(entidades.text, entidades.label_)
-
-    # displacy.render(doc, style = "ent", jupyter=True)
-
     obj_html = displacy.render(doc, style='ent')
-    # obj_html = displacy.parse_deps(doc)
+    
     return obj_html
+
+
+def process_parse_dependency(texto):
+    """
+        Função para retorna à análise de dependência do texto processado
+    Args:
+        texto (string): Texto enviado para ser processado
+
+    Returns:
+        [Objeto]: Objeto html gerado pelo displacy.render
+    """
+    nlp = obj_nlp()
+    doc = nlp(texto)
+
+    obj_html = displacy.render(doc, style='dep')
+
+    return obj_html
+
+def obj_nlp():
+    """
+        Instanciando objet spacy com idioma PT
+    Returns:
+        [Objeto]: Instancia do spacy
+    """
+    nlp = spacy.load('pt')
+    
+    return nlp
